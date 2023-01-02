598967058beeb6f553302540a722890b853e50e0
MacBook <macbook@Gabriel.local>
2023-01-01 23:42:28 -0300

    Agregamos pag de inicio Index.html con el root desde app.js y probamos las apis que muestra todos los productos y busqueda por ID

--- clase6/servidor/app.js
+++ clase6/servidor/app.js
@@ -1,32 +1,49 @@
 const express = require('express');
 const {productos} = require('./src/class/ProductManager');
+const path = require("path")
 
 // Inicializamos el servidor
 const app = express();
 // Levantamos el servidor en un puerto
 app.listen(3000);
+// Configuramos el servidor para que reciba datos complejos desde la url,
+app.use(express.urlencoded({extended:true}));
 
 
 // ENDPOINTS 
 app.get("/", function (req, res){
-  res.send("Servidor arriba");
+  //res.send("Servidor arriba");
+  // indicamos el root del proyecto y concatenamos con el archivo inicial
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-// Endpoint consulta de productos con limit
-app.get('/products', (req, res) => {
-  let products = productos.getProducts();
-  const limit = req.query.limit;
-  if(limit && !isNaN(Number(limit))){
-    const limitNumber = Number(limit);
-    if (limitNumber > 0 && limitNumber <= productos.length) {
-      products = productos.slice(0, limit);
-    } else {
-      console.log('No hay tantos productos disponibles')
-    }
-  }
-  res.send(products);
-});
+// Consulta de productos con limit probamos send y json
+ app.get('/products', (req, res) => {
+   let products = productos.getProducts();
+   const limit = req.query.limit;
+   if(limit && !isNaN(Number(limit))){
+     const limitNumber = Number(limit);
+     if (limitNumber > 0 && limitNumber <= productos.length) {
+       products = productos.slice(0, limit);
+     } else {
+       console.log('No hay tantos productos disponibles')
+     }
+   }
+   //res.send(products);
+   res.json(products)
+ });
 
+
+
+
+// Api de consulta por ID, debe ser un nº entero
+app.get('/products/:code', (req, res) =>{
+  const productID = req.query.code;
+  //console.log(productID);
+  const product = productos.getProductById(productID);
+  
+  res.send(product);
+})
 
 
 
@@ -37,13 +54,13 @@ app.get('/products', (req, res) => {
 
 
 // Endpoint consultando usuario
-app.get('/usuario', (req, res) => {
-  const respuesta = {
-    nombre: "Ele",
-    apellido: "Kunz"
-  };
-  res.send(respuesta);
-} )
+// app.get('/usuario', (req, res) => {
+//   const respuesta = {
+//     nombre: "Ele",
+//     apellido: "Kunz"
+//   };
+//   res.send(respuesta);
+// } )
 
 // app.get('/items/:code', (req, res)=> {
 //   const item = [];
@@ -52,23 +69,4 @@ app.get('/usuario', (req, res) => {
 
 
 
-
-
-
-
-// Servidor en NODE http, no lo vamos a usar
-// const http = require('http');
-
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
 
